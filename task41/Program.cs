048cd63de05aed8a28fab09669d6f041acba3d9c
Olga <olgabutyrina2020@gmail.com>
2023-02-19 00:14:14 +0800

    Added task41

---
+++ task41/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше
+// 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+int [] GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    return arr;
+}
+
+int PositivNum(int [] arr)
+{int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) count += 1;
+}
+return count;
+}
+
+var N = GetNumber("Введите, пожалуйста, числа через пробел: ");
+System.Console.WriteLine("Количество чисел больше 0 = " + PositivNum(N));
